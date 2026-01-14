e2f89799c0d61fcbf0db80d00806783160c005c6
Matt Jensen <mjensen415@gmail.com>
2026-01-13 22:20:05 -0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request: Request): Promise<Response> {
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      });
+    }
+
+    return new Response("ok", {
+      headers: { "Access-Control-Allow-Origin": "*" },
+    });
+  },
+};
